8177aaa9fcf0721dd6a7096e7572409ee37a7ef5
Nikita Smelkov <nikiboy2001rus@gmail.com>
2021-05-22 22:14:51 +0300

    Chinese update

--- script.js
+++ script.js
@@ -39,7 +39,7 @@ var TxtType = function(el, toRotate, period) {
     }, delta);
    };
     
-   var languages = `["It's not time yet", "現在還沒來", "Aún no es tiempo", "अभी व़क्त नहीं हुआ है", "bákoyon báya esía bukáya", "এখনও সময় হয় নি", "Ещё не время", "انها لم يحن الوقت بعد", "Ce n'est pas encore le moment", "まだ時間がありません", "Es ist noch nicht Zeit", "아직 시간이 아니야"]`;
+   var languages = `["It's not time yet", "时间还没到呢", "Aún no es tiempo", "अभी व़क्त नहीं हुआ है", "bákoyon báya esía bukáya", "এখনও সময় হয় নি", "Ещё не время", "انها لم يحن الوقت بعد", "Ce n'est pas encore le moment", "まだ時間がありません", "Es ist noch nicht Zeit", "아직 시간이 아니야"]`;
     
    window.onload = function() {
     var elements = document.getElementsByClassName('typewrite');
